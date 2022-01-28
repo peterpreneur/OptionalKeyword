6491a0a7bba20e7c530c25780e9b3cdb17ae43ee
pas8 <peterpreneur@gmail.com>
2022-01-27 20:30:32 -0600

    optional

--- OptionalDemo/src/OptionalDemo/OptionalDemo.java
+++ OptionalDemo/src/OptionalDemo/OptionalDemo.java
@@ -3,8 +3,17 @@ package OptionalDemo;
 public class OptionalDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Profile profile1 = new Profile("Trevor", "Page");
+		
+		outputProfile(profile1);		
+		
+	}
 
+	private static void outputProfile(Profile profile) {
+		System.out.println("First Name: " + profile.getFirstName().trim());
+		System.out.println("Middle Name: " + profile.getMiddleName().trim());
+		System.out.println("Last Name: " + profile.getLastName().trim());
+		System.out.println("Hobbies: " + profile.getHobbies().trim());
 	}
 
 }
--- OptionalDemo/src/OptionalDemo/Profile.java
+++ OptionalDemo/src/OptionalDemo/Profile.java
@@ -4,6 +4,12 @@ public class Profile {
 	private String firstName;
 	private String middleName;
 	private String lastName;
+	
+	public Profile(String firstName, String middleName) {
+		this.firstName = firstName;
+		this.middleName = middleName;
+	}
+	
 	private String hobbies;
 	
 	public String getFirstName() {
@@ -12,29 +18,32 @@ public class Profile {
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	
 	public String getMiddleName() {
 		return middleName;
 	}
 	public void setMiddleName(String middleName) {
 		this.middleName = middleName;
 	}
+	
 	public String getLastName() {
 		return lastName;
 	}
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	
 	public String getHobbies() {
 		return hobbies;
 	}
 	public void setHobbies(String hobbies) {
 		this.hobbies = hobbies;
 	}
+	
 	@Override
 	public String toString() {
 		return "Profile [firstName=" + firstName + ", middleName=" + middleName + ", lastName=" + lastName
 				+ ", hobbies=" + hobbies + "]";
 	}
-	
 	
 }
